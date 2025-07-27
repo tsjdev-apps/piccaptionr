4da404330faf17d9c78e69bc43098053365c8e31
Sebastian Jensen <apps@tsjdev-apps.de>
2025-07-27 18:37:41 +0200

    Add in-memory activity log service and interface
    Introduces ActivityLogService and IActivityLogService to provide a simple in-memory logging mechanism using ObservableCollection. Enables adding and retrieving log messages for activity tracking within the application.

---
+++ src/PicCaptionr/Services/ActivityLogService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using PicCaptionr.Services.Interfaces;
+
+namespace PicCaptionr.Services;
+
+/// <summary>
+/// Provides a simple in-memory activity log service 
+/// that stores and exposes log messages.
+/// </summary>
+public class ActivityLogService : IActivityLogService
+{
+	// Internal collection to store log entries.
+	readonly ObservableCollection<string> entries = [];
+
+	/// <inheritdoc/>
+	public void AddEntry(string message)
+	{
+		entries.Add(message);
+	}
+
+	/// <inheritdoc/>
+	public ObservableCollection<string> GetEntries()
+		=> entries;
+}
---
+++ src/PicCaptionr/Services/Interfaces/IActivityLogService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PicCaptionr.Services.Interfaces;
+
+/// <summary>
+/// Provides a logging mechanism to store 
+/// and retrieve activity log messages.
+/// </summary>
+public interface IActivityLogService
+{
+	/// <summary>
+	/// Adds a new message to the activity log.
+	/// </summary>
+	/// <param name="message">
+	/// The message to add to the log.</param>
+	void AddEntry(string message);
+
+	/// <summary>
+	/// Retrieves all logged messages as 
+	/// an observable collection.
+	/// </summary>
+	/// <returns>
+	/// An ObservableCollection containing log messages.</returns>
+	ObservableCollection<string> GetEntries();
+}
