432d9963991dae526b4e94b0311df9a21fbed2c6
Sebastian Jensen <apps@tsjdev-apps.de>
2025-07-27 20:15:28 +0200

    Add XML summary to Program entry point
    Introduced an XML documentation comment to the Program class, clarifying its role as the entry point for the Avalonia application. No functional changes were made.

--- src/PicCaptionr.AvaloniaApp/Program.cs
+++ src/PicCaptionr.AvaloniaApp/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using Avalonia;
+﻿using Avalonia;
 
 namespace PicCaptionr.AvaloniaApp;
+
+/// <summary>
+/// The application entry point for the Avalonia app.
+/// Responsible for configuring and launching the 
+/// app using a classic desktop lifetime.
+/// </summary>
 sealed class Program
 {
 	// Initialization code. Don't use any Avalonia, third-party APIs or any
 	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
 	// yet and stuff might break.
 	[STAThread]
-	public static void Main(string[] args) => BuildAvaloniaApp()
-		.StartWithClassicDesktopLifetime(args);
+	public static void Main(string[] args) 
+		=> BuildAvaloniaApp()
+			.StartWithClassicDesktopLifetime(args);
 
 	// Avalonia configuration, don't remove; also used by visual designer.
 	public static AppBuilder BuildAvaloniaApp()
